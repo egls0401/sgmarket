e0c6c27bd85d14890ff643f0100e6e8750c772c6
a960401 <a960401sogang@gmail.com>
2016-09-01 11:10:57 +0000

    remove seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-User.create(id:1, email:'admin@sgmarket.com', password:'rktdbsgud123$', username:'관리자',realname:'어드민', admin:true, userpic:'hugging', confirmable: true)
